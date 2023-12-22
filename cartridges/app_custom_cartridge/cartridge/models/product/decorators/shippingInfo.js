642fe6e9726c185a993e9f5c03aefcb96c27b7fe
Karolina Rachuta <karolina.rachuta@gmail.com>
2023-12-22 18:35:38 +0400

    adding generic content assets

--- cartridges/app_custom_cartridge/cartridge/models/product/decorators/shippingInfo.js
+++ cartridges/app_custom_cartridge/cartridge/models/product/decorators/shippingInfo.js
@@ -5,11 +5,16 @@
 function getShippingInformation(productID) {
     var ContentMgr = require('dw/content/ContentMgr');
     var shippingContentAsset = 'shipping-info-' + productID;
+    var genericContentAsset = 'shipping-info-generic';
     var shippingContent = ContentMgr.getContent(shippingContentAsset);
+    if (shippingContentAsset && shippingContentAsset.online) {
+        return shippingContent.custom.body;
+    } else {
+        return genericContentAsset.custom.body;
+    }
  // checks: if shipping conent.online if custom.body then asignt to variable // not avaiable get generiv conetn asset and aisgn it to shipping content
-
-    return shippingContent.custom.body;
 }
+
 module.exports = function (object, apiProduct) {
     var productID = apiProduct.master ? apiProduct.ID : apiProduct.masterProduct.ID;
     Object.defineProperty(object, 'shippingInfo', {
