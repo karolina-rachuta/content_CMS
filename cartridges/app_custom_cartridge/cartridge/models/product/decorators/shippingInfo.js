2bcb48717811634fc34dcb6ee6a916a0f7d2c81d
Karolina Rachuta <karolina.rachuta@gmail.com>
2023-12-22 20:20:36 +0400

    finished pdp taks

--- cartridges/app_custom_cartridge/cartridge/models/product/decorators/shippingInfo.js
+++ cartridges/app_custom_cartridge/cartridge/models/product/decorators/shippingInfo.js
@@ -1,16 +1,24 @@
 'use strict';
 
-
-
+/**
+ * Get list price for a product
+ * @param {string} productID- Product ID
+ * @param {dw.catalog.Product} apiProduct - Product information returned by the script API
+ * @param {Object} options - Options passed in from the factory
+ * @property {string} apiProduct.ID - Variables passed
+ * @property {string} apiProduct.masterProduct.ID - Variables passed
+ * @return {string} - Content Asset Body
+ */
 function getShippingInformation(productID) {
     var ContentMgr = require('dw/content/ContentMgr');
     var shippingContentAsset = 'shipping-info-' + productID;
-    var genericContentAsset = 'shipping-info-generic';
     var shippingContent = ContentMgr.getContent(shippingContentAsset);
-    if (shippingContentAsset && shippingContentAsset.online) {
+    if (shippingContent && shippingContent.online) {
         return shippingContent.custom.body;
     } else {
-        return genericContentAsset.custom.body;
+        var genericContentAsset = 'shipping-info-generic';
+        var genericContent = ContentMgr.getContent(genericContentAsset);
+        return genericContent.custom.body;
     }
  // checks: if shipping conent.online if custom.body then asignt to variable // not avaiable get generiv conetn asset and aisgn it to shipping content
 }
--- cartridges/app_custom_cartridge/cartridge/models/product/fullProduct.js
+++ cartridges/app_custom_cartridge/cartridge/models/product/fullProduct.js
@@ -7,12 +7,6 @@ var base = module.superModule;
  * @param {Object} product - Product Model to be decorated
  * @param {dw.catalog.Product} apiProduct - Product information returned by the script API
  * @param {Object} options - Options passed in from the factory
- * @property {dw.catalog.ProductVarationModel} options.variationModel - Variation model returned by the API
- * @property {Object} options.options - Options provided on the query string
- * @property {dw.catalog.ProductOptionModel} options.optionModel - Options model returned by the API
- * @property {dw.util.Collection} options.promotions - Active promotions for a given product
- * @property {number} options.quantity - Current selected quantity
- * @property {Object} options.variables - Variables passed in on the query string
  *
  * @returns {Object} - Decorated product model
  */
